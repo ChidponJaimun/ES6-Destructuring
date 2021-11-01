9911f33f57ac6c16e354ffb42dae13199f2765fd
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-02 00:09:26 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import carData from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+// import animals, { useAnimals } from "./data";
+
+// const [cat, dog] = animals;
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+// console.log(cat.name);
+// console.log(cat.sound);
+// console.log(dog.name);
+// console.log(dog.sound);
+
+const [tesla, honda] = carData;
+const teslaTopSpeed = tesla.speedStats.topSpeed;
+const teslaTopColour = tesla.coloursByPopularity;
+
+const hondaTopSpeed = honda.speedStats.topSpeed;
+const hondaTopColour = honda.coloursByPopularity;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
